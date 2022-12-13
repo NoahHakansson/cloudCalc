cb08b7218c67dacc57c7ac0ad502dd8a384f5fe3
Noah Hakansson <noah.hakansson@protonmail.com>
2022-12-13 14:48:42 +0100

    Add extra help function and added another multiplication test.

--- backend/src/server/server_test.go
+++ backend/src/server/server_test.go
@@ -13,6 +13,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func setupContext() (*gin.Context, *httptest.ResponseRecorder) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	// make sure c.Request is not nil
+	c.Request = &http.Request{
+		Header: make(http.Header),
+	}
+
+	return c, w
+}
+
 func mockJSONPost(c *gin.Context, content interface{}) {
 	c.Request.Method = "POST"
 	c.Request.Header.Set("Content-Type", "application/json")
@@ -28,50 +40,50 @@ func mockJSONPost(c *gin.Context, content interface{}) {
 	c.Request.Body = io.NopCloser(bytes.NewBuffer(jsonbytes))
 }
 
-func TestSomething(t *testing.T) {
-	// assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
-
-	// assert inequality
-	assert.NotEqual(t, 123, 456, "they should not be equal")
-
-	// assert for nil (good for errors)
-	assert.Nil(t, nil)
-
-	// assert for not nil (good when you expect something)
-	if assert.NotNil(t, nil) {
-		// now we know that object isn't nil, we are safe to make
-		// further assertions without causing any errors
-		assert.Equal(t, "Something", "Something")
-	}
-}
-
 func TestMultiplication(t *testing.T) {
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
+	t.Run("Both positive", func(t *testing.T) {
+		c, w := setupContext()
+		// Set Body, Header and Content-Type
+		mockJSONPost(c, &calcRequest{
+			First:    2,
+			Second:   3,
+			Operator: "x",
+		})
 
-	// make sure c.Request is not nil
-	c.Request = &http.Request{
-		Header: make(http.Header),
-	}
+		// expected response
+		expectedJSON := gin.H{
+			"result": 6,
+			"status": "success",
+		}
+		expected, _ := json.Marshal(expectedJSON)
 
-	// Set Body, Header and Content-Type
-	mockJSONPost(c, &calcRequest{
-		First:    2,
-		Second:   3,
-		Operator: "x",
+		// call API endpoint
+		calcEndpoint(c)
+
+		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, string(expected), w.Body.String())
 	})
 
-	// expected response
-	expectedJSON := gin.H{
-		"result": 6,
-		"status": "success",
-	}
-	expected, _ := json.Marshal(expectedJSON)
+	t.Run("first negative", func(t *testing.T) {
+		c, w := setupContext()
+		// Set Body, Header and Content-Type
+		mockJSONPost(c, &calcRequest{
+			First:    -2,
+			Second:   3,
+			Operator: "x",
+		})
 
-	// call API endpoint
-	calcEndpoint(c)
+		// expected response
+		expectedJSON := gin.H{
+			"result": -6,
+			"status": "success",
+		}
+		expected, _ := json.Marshal(expectedJSON)
 
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, string(expected), w.Body.String())
+		// call API endpoint
+		calcEndpoint(c)
+
+		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, string(expected), w.Body.String())
+	})
 }
