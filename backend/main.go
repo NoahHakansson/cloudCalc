e6fff9e140805c491366ca7640f89af55a501a9c
Flex <noah.hakansson@protonmail.com>
2022-12-25 14:59:29 +0100

    Math server GIN API (#1)
    * Started on math server.
    
    * Basic calculator API endpoint is done.
    
    * Test for multiplication done
    
    * Add extra help function and added another multiplication test.

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,21 @@
 // Package main
 package main
 
+import (
+	"log"
+
+	"github.com/NoahHakansson/cloudCalc/backend/src/server"
+)
+
 func main() {
+	r, err := server.Start()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = r.Run(":5000")
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
--- backend/src/server/server.go
+++ backend/src/server/server.go
@@ -1,2 +1,92 @@
 // Package server provides server
 package server
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// variables
+
+var errNotSupportedOperator = errors.New("Error: Operator is not supported")
+
+// structs
+
+type calcRequest struct {
+	First    float64 `form:"first" xml:"first" json:"first"`
+	Second   float64 `form:"second" xml:"second" json:"second"`
+	Operator string  `form:"operator" xml:"operator" json:"operator"`
+}
+
+// Start function
+//
+// Starts server or returns an error.
+func Start() (*gin.Engine, error) {
+	r := gin.Default()
+	// set trusted proxy to localhost
+	err := r.SetTrustedProxies([]string{"127.0.0.1"})
+	if err != nil {
+		return nil, err
+	}
+
+	// routes
+	r.POST("/api/calc", calcEndpoint)
+
+	return r, nil
+}
+
+func calcEndpoint(c *gin.Context) {
+	calcReq := calcRequest{}
+	// bind body data or return error if it fails
+	if err := c.ShouldBind(&calcReq); err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// calculate result
+	result, err := calculate(calcReq.First, calcReq.Second, calcReq.Operator)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":    err.Error(),
+			"first":    calcReq.First,
+			"second":   calcReq.Second,
+			"operator": calcReq.Operator,
+		})
+		return
+	}
+	// return result
+	c.JSON(http.StatusOK, gin.H{
+		"status": "success",
+		"result": result,
+	})
+}
+
+// calculates the math operation given
+// uses a switch case internally.
+// Returns an error if operator is not recognised.
+func calculate(first float64, second float64, operator string) (float64, error) {
+	var result float64
+	switch operator {
+	// cases break automatically, no fallthrough by default
+	case "x":
+		fmt.Println("Multiplication")
+		result = first * second
+	case "/":
+		fmt.Println("Division")
+		result = first / second
+	case "+":
+		fmt.Println("Addition")
+		result = first + second
+	case "-":
+		fmt.Println("Subtraction")
+		result = first - second
+	default:
+		return 0, errNotSupportedOperator
+	}
+	return result, nil
+}
